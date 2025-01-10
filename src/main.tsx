f4f4d572cc764199677f0db7b4d9e772a5e07d1b
ChrisOlmedo <alejandro.olmedo14@gmail.com>
2025-01-09 19:34:47 -0600

    Implementacion de analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css' //reset css
 import App from './App.tsx'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SpeedInsights />
+    <Analytics />
     <BrowserRouter>
       <App />
     </BrowserRouter>
